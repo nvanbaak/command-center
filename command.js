34a5a068ef2839dece0faa76e2e400580f51fc4e
Nikolai <nvanbaak@gmail.com>
2020-09-01 09:46:03 -0700

    same as last commit but this time I remember to track the javascript file

---
+++ command.js
@@ -0,0 +1,17 @@
+
+
+function bootcampButton() {
+    window.open("https://bootcampspot.com/");
+}
+
+function gitButton() {
+    window.open("https://github.com/nvanbaak?tab=repositories");
+}
+
+function mdnButton() {
+    window.open("https://developer.mozilla.org/en-US/docs/Web");
+}
+
+function w3Button() {
+    window.open("https://www.w3schools.com/js/default.asp");
+}
