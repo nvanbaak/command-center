e8c084bf41aa9de3bfbfedbdd54b606ed94899c7
Nikolai <nvanbaak@gmail.com>
2020-09-01 17:54:59 -0700

    Refactored javascript to use one function across all buttons.

--- command.js
+++ command.js
@@ -1,17 +1,6 @@
 
 
-function bootcampButton() {
-    window.open("https://bootcampspot.com/");
-}
-
-function gitButton() {
-    window.open("https://github.com/nvanbaak?tab=repositories");
-}
-
-function mdnButton() {
-    window.open("https://developer.mozilla.org/en-US/docs/Web");
-}
-
-function w3Button() {
-    window.open("https://www.w3schools.com/js/default.asp");
+function linkButton(url) {
+    //var dest = "https://" + url;
+    window.open(url);
 }
