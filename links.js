a04d725a7d0964493831dc8e70157f8889ce65b3
Nikolai <nvanbaak@gmail.com>
2021-01-29 20:30:54 -0800

    links are dynamically displayed

--- links.js
+++ links.js
@@ -3,25 +3,36 @@ const projectLinks = $("#project-tab");
 const linkArr = [{text:"Command Center",href:"https://github.com/nvanbaak/command-center"},{text:"Pocket Butler",href:"https://github.com/nvanbaak/pocket-butler"},{text:"The Archivist",href:"https://github.com/nvanbaak/the-archivist"},{text:"Cuisine",href:"https://github.com/nvanbaak/cuisine"},{text:"Medival Town Simulator",href:"https://github.com/nvanbaak/medieval-town-simulator"}]
 
 
+populateLinks();
 
 // Adds the links in a grind format
 function populateLinks() {
 
     let remainingLinks = linkArr.length;
-    let beginningLine = true;
 
     for (let i = 0; i < linkArr.length; i += 2) {
 
+        // Make a new row
         let newRow = $("<div>", {"class":"arrayRow"})
 
-        newRow.append( $("a") )
+        // Add the first link
+        newRow.append( $("<a>", {"text":linkArr[i].text, "href":linkArr[i].href}) )
 
+        // if we didn't just add the last link, add the next one
+        if (remainingLinks > 1) {
 
+            // First add separator
+            newRow.append($("<div>", {"class":"arraySeparator"}))
 
+            // Then the actual link
+            newRow.append( $("<a>", {"text":linkArr[i+1].text, "href":linkArr[i+1].href}) )
+        }
 
+        // Append the new row
+        projectLinks.append(newRow);
 
-
-
+        // Count down the remaining links
+        remainingLinks -= 2;
     }
 
 
