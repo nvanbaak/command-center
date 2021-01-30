d9c9402474e12e4d73d980988cccd2b06ee74864
Nikolai <nvanbaak@gmail.com>
2021-01-29 20:49:12 -0800

    abstracted the link population method to also work on launch panel

--- links.js
+++ links.js
@@ -1,22 +1,27 @@
 const projectLinks = $("#project-tab");
+const launchLinks = $("#launch-tab");
 
-const linkArr = [{text:"Command Center",href:"https://github.com/nvanbaak/command-center"},{text:"Pocket Butler",href:"https://github.com/nvanbaak/pocket-butler"},{text:"The Archivist",href:"https://github.com/nvanbaak/the-archivist"},{text:"Cuisine",href:"https://github.com/nvanbaak/cuisine"},{text:"Medival Town Simulator",href:"https://github.com/nvanbaak/medieval-town-simulator"}]
+const projectLinkArr = [{text:"Command Center",href:"https://github.com/nvanbaak/command-center"},{text:"Pocket Butler",href:"https://github.com/nvanbaak/pocket-butler"},{text:"The Archivist",href:"https://github.com/nvanbaak/the-archivist"},{text:"Cuisine",href:"https://github.com/nvanbaak/cuisine"},{text:"Medival Town Simulator",href:"https://github.com/nvanbaak/medieval-town-simulator"}]
 
 
-populateLinks();
+const launchLinkArr = [{text:"Github",href:"https://github.com/nvanbaak"},{text:"Bootstrap Docs",href:"https://getbootstrap.com/docs/4.5/getting-started/introduction/"},{text:"LinkedIn",href:"https://www.linkedin.com/in/nick-van-baak/"},{text:"Portfolio",href:"https://nvanbaak.github.io/profile-page/"}]
 
-// Adds the links in a grind format
-function populateLinks() {
 
-    let remainingLinks = linkArr.length;
+populateLinks(projectLinks, projectLinkArr);
+populateLinks(launchLinks, launchLinkArr);
 
-    for (let i = 0; i < linkArr.length; i += 2) {
+// Adds the given links in a grid format
+function populateLinks(appendHere, givenArray) {
+
+    let remainingLinks = givenArray.length;
+
+    for (let i = 0; i < givenArray.length; i += 2) {
 
         // Make a new row
         let newRow = $("<div>", {"class":"arrayRow"})
 
         // Add the first link
-        newRow.append( $("<a>", {"text":linkArr[i].text, "href":linkArr[i].href}) )
+        newRow.append( $("<a>", givenArray[i]) )
 
         // if we didn't just add the last link, add the next one
         if (remainingLinks > 1) {
@@ -25,11 +30,11 @@ function populateLinks() {
             newRow.append($("<div>", {"class":"arraySeparator"}))
 
             // Then the actual link
-            newRow.append( $("<a>", {"text":linkArr[i+1].text, "href":linkArr[i+1].href}) )
+            newRow.append( $("<a>", givenArray[i+1]) )
         }
 
         // Append the new row
-        projectLinks.append(newRow);
+        appendHere.append(newRow);
 
         // Count down the remaining links
         remainingLinks -= 2;
